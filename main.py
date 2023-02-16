7cbddfac2cdac4bf4f7b81ce8e03fc866af8a910
Juan Carlos Neíl Palacios Escobar <93744783+JuanK003@users.noreply.github.com>
2023-02-16 10:42:52 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+# Importing the functions from the modules.
+from pkg.mod_1 import func_1, func_2
+from pkg.mod_2 import func_3, func_4
+
+print(func_1())
+
+print(func_2())
+
+print(func_3())
+
+print(func_4())
+
+'''
+
+# Importing the module `pkg` and then printing the variable `URL` from the module.
+import pkg
+
+print(pkg.URL)
+print(pkg.mod_1.func_1())
